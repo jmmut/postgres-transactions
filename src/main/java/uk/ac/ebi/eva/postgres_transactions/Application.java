80714c0d09786f919759a15c34625cc5c410a746
jose miguel mut <jomutlo@gmail.com>
2019-05-31 11:33:00 +0100

    add reset

--- src/main/java/uk/ac/ebi/eva/postgres_transactions/Application.java
+++ src/main/java/uk/ac/ebi/eva/postgres_transactions/Application.java
@@ -25,22 +25,38 @@ public class Application {
 
     public static void main(String[] args) throws InterruptedException, SQLException {
 //        System.out.println("args provided: " + String.join(", ", args));
-        if (args.length != 1) {
-            System.out.println("need 1 parameter, the url in format jdbc:postgresql://localhost:port/db?user=fred&password=secret");
+        if (args.length != 1 && args.length != 2) {
+            System.out.println("need 1 or 2 parameters, the url in format jdbc:postgresql://localhost:port/db?user=fred&password=secret and 'reset' to reset the DB");
             return;
         }
         String url = args[0];
         Connection conn = DriverManager.getConnection(url);
-        conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
-        conn.setAutoCommit(false);
-        PreparedStatement s = conn.prepareStatement("select max(counter) counter from transaction_test");
-        ResultSet resultSet = s.executeQuery();
-        resultSet.next();
-        long counter = resultSet.getLong("counter");
-        System.out.println(counter);
-        conn.prepareStatement("insert into transaction_test values (?)").setLong(1, counter +1);
-        conn.commit();
-        conn.close();
+        if (args.length == 2) {
+            if (!args[1].equals("reset")) {
+                System.out.println("the second parameter can only be 'reset'");
+                return;
+            }
+            conn.prepareStatement("delete from transaction_test where counter > 1").executeUpdate();
+            conn.close();
+            System.out.println("table reset to have only 1 row with counter=1");
+        } else {
+            conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+            conn.setAutoCommit(false);
+            PreparedStatement preparedQuery = conn.prepareStatement("select max(counter) counter from transaction_test");
+            ResultSet resultSet = preparedQuery.executeQuery();
+            resultSet.next();
+            long counter = resultSet.getLong("counter");
+            System.out.println("max counter: " + counter);
+            System.out.println("waiting before update (within transaction)");
+            Thread.sleep(2000);
+            long incrementedCounter = counter + 1;
+            System.out.println("inserting counter=" + incrementedCounter);
+            PreparedStatement preparedInsert = conn.prepareStatement("insert into transaction_test values (?)");
+            preparedInsert.setLong(1, incrementedCounter);
+            preparedInsert.executeUpdate();
+            conn.commit();
+            conn.close();
+            System.out.println("connection commited and closed");
+        }
     }
-
 }
